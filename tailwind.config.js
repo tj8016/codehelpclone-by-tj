4838d99128d651ff40ee2b0bc2e16498d5bfa530
Tohidujjaman Hoque <tohi7479@gmail.com>
2023-03-20 22:40:14 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+      },
+      colors: {
+        firstColor: "#6674cc",
+        secondColor: "#363a45",
+        thirdColor: "#4a4f5c",
+        subText: "#777c85",
+      },},
+  },
+  plugins: [],
+}
